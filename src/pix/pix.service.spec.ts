06ea27ff37d30c144350ec57f2daa723ba701ad5
Gabriel Sartorato <gabriel.sartorato@hotmail.com>
2022-04-09 15:45:13 -0300

    test: ensure pix service return all pending transactions

--- src/pix/pix.service.spec.ts
+++ src/pix/pix.service.spec.ts
@@ -1,18 +1,59 @@
+import { HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
+import { of, throwError } from 'rxjs';
+import { ITransaction } from './interfaces/transaction.interface';
 import { PixService } from './pix.service';
 
 describe('PixService', () => {
-  let service: PixService;
+  let pixService: PixService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PixService],
+      providers: [
+        PixService,
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<PixService>(PixService);
+    pixService = module.get<PixService>(PixService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(pixService).toBeDefined();
+  });
+
+  describe('getAllContingencyTransaction', () => {
+    it('should be able get all pending transactions', async () => {
+      const expected: ITransaction[] = [
+        {
+          customerID: '6b229ef3-e1fe-41e2-8c8c-fbb9e731a6f6',
+          email: 'saraigrady@quigley.net',
+          key: '681.512.145-38',
+          value: 1541.96,
+          bank: null,
+        },
+      ];
+
+      jest.spyOn(httpService, 'get').mockReturnValueOnce(
+        of({
+          status: 200,
+          statusText: 'OK',
+          config: {},
+          headers: {},
+          data: expected,
+        }),
+      );
+
+      const result = await pixService.getAllContingencyTransaction();
+
+      expect(result).toEqual(expected);
+    });
   });
 });
--- src/pix/pix.service.ts
+++ src/pix/pix.service.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { transformPixValidateKeyFactory } from '../utils/transformPixValidateKeyFactory';
 import { IBalance } from './interfaces/balance.interface';
 import { ICustomer, ICustomerBank } from './interfaces/customer.interface';
@@ -10,34 +10,63 @@ export class PixService {
   constructor(private readonly httpService: HttpService) {}
 
   async resendContingencyList() {
-    const customersList = await this.getAllBanksAndClients();
+    try {
+      const customersList = await this.getAllBanksAndClients();
 
-    const pendingTransactions = await this.getAllContingencyTransaction();
-
-    const balance = await this.getBalance();
-
-    const preparedToSendTransaction = pendingTransactions
-      .map((transaction) => {
-        const customer = customersList.find(
-          (customer) => customer.customerID === transaction.customerID,
+      if (customersList.length === 0) {
+        throw new HttpException(
+          'Lista de clientes não encontrada',
+          HttpStatus.NOT_FOUND,
         );
+      }
 
-        const key = transformPixValidateKeyFactory(transaction.key);
+      const pendingTransactions = await this.getAllContingencyTransaction();
 
-        return {
-          transaction: {
-            ...transaction,
-            key: key,
-            bank: {
-              agency: customer.agency,
-              account: customer.account,
-            },
-          },
-        };
-      })
-      .filter((transaction) => transaction.transaction.value < balance);
+      if (pendingTransactions.length === 0) {
+        throw new HttpException(
+          'Não há transações a serem processadas.',
+          HttpStatus.NOT_FOUND,
+        );
+      }
 
-    return { preparedToSendTransaction };
+      const transFormTransaction = await Promise.all(
+        pendingTransactions.map(async (transaction) => {
+          const customer = customersList.find(
+            (customer) => customer.customerID === transaction.customerID,
+          );
+
+          const key = transformPixValidateKeyFactory(transaction.key);
+
+          if (customer.customerID) {
+            const balance = await this.getBalance();
+
+            if (transaction.value < balance) {
+              return {
+                transaction: {
+                  ...transaction,
+                  key: key,
+                  bank: {
+                    agency: customer.agency,
+                    account: customer.account,
+                  },
+                },
+              };
+            }
+          }
+        }),
+      );
+
+      const preparedToSendTransactions = transFormTransaction.filter(
+        (tran) => tran,
+      );
+
+      return { preparedToSendTransactions };
+    } catch (error) {
+      throw new HttpException(
+        'Transações não realizadas devido erro interno.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async getAllContingencyTransaction(): Promise<ITransaction[]> {
@@ -74,7 +103,6 @@ export class PixService {
         'https://run.mocky.io/v3/e0f453b7-620c-4479-839e-28ac58111fca',
       )
       .toPromise();
-    console.log(data.balance);
 
     return data.balance;
   }
