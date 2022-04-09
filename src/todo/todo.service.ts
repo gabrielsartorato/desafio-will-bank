956b69ef533a4582b878d6801a723f74c8fbc770
Gabriel Sartorato <gabriel.sartorato@hotmail.com>
2022-04-09 13:11:59 -0300

    feat: add http module

---
+++ src/todo/todo.service.ts
@@ -0,0 +1,22 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { ITodo } from './interfaces/todo.interface';
+
+@Injectable()
+export class TodoService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async getAllTodos() {
+    let todos: ITodo[] = [];
+    const url = 'https://jsonplaceholder.typicode.com/todos';
+
+    const { status, data } = await this.httpService
+      .get<ITodo[]>(url)
+      .toPromise();
+
+    if (status === 200) {
+      todos = data;
+    }
+    return todos;
+  }
+}
